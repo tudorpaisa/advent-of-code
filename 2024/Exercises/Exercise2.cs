cec96a43fd1459c5f49d1a86a109403529d239dd
Tudor Paisa <tudor.paisa@gmail.com>
2024-12-02 11:05:13 +0100

    Day 2 p 2

--- 2024/Exercises/Exercise2.cs
+++ 2024/Exercises/Exercise2.cs
@@ -13,7 +13,7 @@ public class Exercise2 : IExercise
         List<List<int>> reportsList = ParseLevels(input);
         var safeLevels = CheckSafety(reportsList, 3, 3);
 
-        return new Result(safeLevels.Where(i => i ==  true).Count(), false);
+        return new Result(safeLevels.Where(i => i ==  true).Count(), true);
     }
 
     private List<bool> CheckSafety(List<List<int>> reports, int maxIncrease, int maxDecrease)
@@ -42,7 +42,6 @@ public class Exercise2 : IExercise
                     maxIncDiff = Math.Max(maxIncDiff, Math.Abs(diff));
                 }
             }
-            // Console.WriteLine($"{maxIncDiff}, {maxDecDiff}");
 
             if (maxIncDiff > 0 && maxDecDiff > 0)
             {
@@ -87,57 +86,89 @@ public class Exercise2 : IExercise
         var input = File.ReadAllLines(inputFile);
 
         List<List<int>> reportsList = ParseLevels(input);
-        var safeLevels = CheckSafety2(reportsList, 3, 3);
+        var safeLevels = CheckSafety2(reportsList, 3, 3, true);
 
-        return new Result(safeLevels.Where(i => i ==  true).Count(), false);
-
-        return new Result("", false);
+        return new Result(safeLevels.Where(i => i ==  true).Count(), true);
     }
 
-    private List<bool> CheckSafety2(List<List<int>> reports, int maxIncrease, int maxDecrease)
+    private (bool, int) CheckSingleReport(List<int> report, int maxIncrease, int maxDecrease, bool retry)
+    {
+        List<int> diffResults = new();
+
+        for (var i = 0; i < report.Count() - 1; i++)
+        {
+            int? lastResult = null;
+            bool resultSame = false;
+            var diff = report[i] - report[i+1];
+            int result;
+
+            if (diff > 0 && diff <= maxDecrease) {
+                result = -1;
+                if (lastResult == null)
+                {
+                    lastResult = -1;
+                }
+                else
+                {
+                    resultSame = lastResult == -1;
+                    lastResult = -1;
+                }
+            }
+            else if (diff < 0 && Math.Abs(diff) <= maxIncrease)
+            {
+                result = 1;
+                if (lastResult == null)
+                {
+                    lastResult = 1;
+                }
+                else
+                {
+                    resultSame = lastResult == 1;
+                    lastResult = 1;
+                }
+            }
+            else
+            {
+                result = 0;
+                if (lastResult == null)
+                {
+                    lastResult = 0;
+                }
+                else
+                {
+                    resultSame = lastResult == 0;
+                    lastResult = 0;
+                }
+            }
+
+            diffResults.Add(result);
+
+            if (!resultSame && retry)
+            {
+                (var try1Safety, var v1) = CheckSingleReport(report.Where((v, idx) => idx != i).ToList(), maxIncrease, maxDecrease, false);
+                (var try2Safety, var v2) = CheckSingleReport(report.Where((v, idx) => idx != i + 1).ToList(), maxIncrease, maxDecrease, false);
+                if (try1Safety == true)
+                {
+                    return (try1Safety, v1);
+                }
+                else if (try2Safety == true)
+                {
+                    return (try2Safety, v2);
+                }
+            }
+        }
+        var dist = diffResults.Distinct();
+        var safetyCheck = dist.Count() == 1 && !dist.Contains(0);
+        return (safetyCheck, dist.First());
+    }
+
+    private List<bool> CheckSafety2(List<List<int>> reports, int maxIncrease, int maxDecrease, bool retry)
     {
         List<bool> safeReports = new();
 
         foreach (var report in reports)
         {
-            var maxIncDiff = 0;
-            var maxDecDiff = 0;
-
-            for (var i = 0; i < report.Count() - 1; i++)
-            {
-                var diff = report[i] - report[i+1];
-                if (diff == 0)
-                {
-                    maxDecDiff = 99;
-                    maxIncDiff = 99;
-                }
-                else if (diff > 0)
-                {
-                    maxDecDiff = Math.Max(maxDecDiff, Math.Abs(diff));
-                }
-                else
-                {
-                    maxIncDiff = Math.Max(maxIncDiff, Math.Abs(diff));
-                }
-            }
-            // Console.WriteLine($"{maxIncDiff}, {maxDecDiff}");
-
-            if (maxIncDiff > 0 && maxDecDiff > 0)
-            {
-                safeReports.Add(false);
-            }
-            else if (maxIncDiff == 0 && maxDecDiff > 0 && maxDecDiff <= maxDecrease)
-            {
-                safeReports.Add(true);
-            }
-            else if (maxDecDiff == 0 && maxIncDiff > 0 && maxIncDiff <= maxIncrease)
-            {
-                safeReports.Add(true);
-            }
-            else
-            {
-                safeReports.Add(false);
-            }
+            safeReports.Add(CheckSingleReport(report, maxIncrease, maxDecrease, retry).Item1);
         }
 
         return safeReports;
