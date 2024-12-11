f282e34a24ad21cee4f45e3c7c984583a3790e60
Tudor Paisa <tudor.paisa@gmail.com>
2024-12-11 20:58:24 +0100

    day 11 2/2

--- 2024/Exercises/Exercise11.cs
+++ 2024/Exercises/Exercise11.cs
@@ -4,18 +4,19 @@ public class Exercise11 : IExercise
 {
     public int GetDay() => 11;
 
-    private List<int> Parse(string input)
+    private List<long> Parse(string input)
     {
-        List<int> parsed = new();
+        List<long> parsed = new();
         foreach (var raw in input.Split(" "))
         {
-            int.TryParse(raw, out var n);
+            long.TryParse(raw, out var n);
             parsed.Add(n);
         }
         return parsed;
     }
 
-    private void Iterate(List<int> stones)
+    // Brute-force solution
+    private void Iterate(List<long> stones)
     {
         var i = 0;
         var stonesCount = stones.Count();
@@ -32,8 +33,8 @@ public class Exercise11 : IExercise
                 var leftRaw = strNum[0..mid];
                 var rightRaw = strNum[mid..strNum.Count()];
 
-                int.TryParse(leftRaw, out var left);
-                int.TryParse(rightRaw, out var right);
+                long.TryParse(leftRaw, out var left);
+                long.TryParse(rightRaw, out var right);
                 stones[i] = right;
                 stones.Insert(i, left);
 
@@ -45,6 +46,52 @@ public class Exercise11 : IExercise
                 stones[i] = stones[i] * 2024;
             }
             i++;
+        }
+    }
+
+    // Optimized solution
+    private void Iterate(Dictionary<long, long> stoneCount)
+    {
+        Dictionary<long, long> summation = new();
+        var copy = stoneCount.ToDictionary();
+        foreach(var kvp in copy)
+        {
+            var key = kvp.Key;
+            var val = kvp.Value;
+            if (val == 0) continue;
+
+            if (key == 0)
+            {
+                var newKey = 1;
+                summation[newKey] = summation.GetValueOrDefault(newKey, 0) + val;
+                stoneCount[key] = 0;
+            }
+            else if (key.ToString().Count() % 2 == 0)
+            {
+                var strNum = key.ToString();
+                int mid = strNum.Count() / 2;
+                var leftRaw = strNum[0..mid];
+                var rightRaw = strNum[mid..strNum.Count()];
+
+                long.TryParse(leftRaw, out var left);
+                long.TryParse(rightRaw, out var right);
+
+                summation[left] = summation.GetValueOrDefault(left, 0) + val;
+                summation[right] = summation.GetValueOrDefault(right, 0) + val;
+
+                stoneCount[key] = 0;
+            }
+            else
+            {
+                var newKey = key * 2024;
+                summation[newKey] = summation.GetValueOrDefault(newKey, 0) + val;
+                stoneCount[key] = 0;
+            }
+        }
+
+        foreach (var kvp in summation)
+        {
+            stoneCount[kvp.Key] = stoneCount.GetValueOrDefault(kvp.Key, 0) + kvp.Value;
         }
     }
 
@@ -65,9 +112,19 @@ public class Exercise11 : IExercise
     public Result ExecutePart2(string inputFile)
     {
         var input = File.ReadAllLines(inputFile);
-        var parsed = Parse(input.First());
+        var stones = Parse(input.First());
+        Dictionary<long, long> stoneCount = new();
 
+        foreach (var i in stones)
+        {
+            stoneCount[i] = 1;
+        }
 
-        return new Result();
+        for (var i = 0; i < 75; i++)
+        {
+            Iterate(stoneCount);
+        }
+
+        return new Result(stoneCount.Values.Sum(), false);
     }
 }
